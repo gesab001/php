d52a93a7491501d61d2e765a1b32baae9f895710
gesab001 <144000giovanni@gmail.com>
2021-06-16 19:09:09 +0000

    added download progress feature

---
+++ youtube/checkfilesize.php
@@ -0,0 +1,9 @@
+<?php
+
+// get the q parameter from URL
+$q = $_REQUEST["filename"];
+$filename = str_replace(" ","_",$_GET['filename']) . ".mp4.part";
+
+// Output "no suggestion" if no hint was found or output correct values
+echo filesize($filename);
+?>
--- youtube/download.php
+++ youtube/download.php
@@ -1,25 +1,15 @@
 <?php
 
+
 $filename = str_replace(" ","_",$_GET['filename']) . ".mp4";
+
 $videoId =  $_GET['videoId'];
 
-echo "videoId: " . $videoId;
-echo "\n";
-echo "filename: " . $filename;
-echo "\n";
-echo "downloading...please wait";
-echo "\n";
-$command = "youtube-dl -f 18 https://www.youtube.com/watch?v=" . $videoId. " --output " . $filename;
 $command = "youtube-dl -f 18 " . $videoId . " --output " . $filename;
 
 $output = shell_exec($command);
-echo "<pre>$output</pre>";
 
-$command = "INSERT INTO DOWNLOADS  
-include 'insert_table.php';
+echo ("<a href=" . $filename . " download=" . $filename . ">Save downloaded file.</a>");
+
 ?>
-
-<html>
-<a href="<?php echo $filename;?>" download="<?php echo $filename;?>">Download</a>
-</html>
 
