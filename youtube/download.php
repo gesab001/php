f8e934bd558575bee641c204471ff1de6a733a39
root <root@phpmysql.dfheyxdluegevmrpcihrot5wjf.px.internal.cloudapp.net>
2021-06-17 14:05:04 +0000

    added *.mp4 to gitignore

--- youtube/download.php
+++ youtube/download.php
@@ -5,7 +5,6 @@ $filename = str_replace(" ","_",$_GET['filename']) . ".mp4";
 
 $videoId =  $_GET['videoId'];
 
-$filename = "../../downloads/" . $filename;
 $command = "youtube-dl -f 18 " . $videoId . " --output " . $filename;
 
 $output = shell_exec($command);
