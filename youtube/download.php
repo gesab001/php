3a49dfd09235a1331b8164ab877b1f19aeb7a13c
root <root@phpmysql.dfheyxdluegevmrpcihrot5wjf.px.internal.cloudapp.net>
2021-06-17 15:32:20 +0000

    linked holytube

--- youtube/download.php
+++ youtube/download.php
@@ -2,14 +2,24 @@
 
 
 $filename = str_replace(" ","_",$_GET['filename']) . ".mp4";
-
+$filename = str_replace("|", "_", $filename);
 $videoId =  $_GET['videoId'];
 
 $command = "youtube-dl -f 18 " . $videoId . " --output " . $filename;
+$output=null;
+$resultcode = null;
+exec($command, $output, $resultcode);
 
-$output = shell_exec($command);
+if ($resultcode==0){
+ echo ("<a href=$filename download=$filename><h2>$filename</h2></a><br>");
 
-echo ("<a href=" . $filename . " download=" . $filename . ">Save downloaded file.</a>");
-
+ $files = glob('*.mp4');
+ $sorted_array = $files;
+ echo ("<h3>Other downloaded videos</h2>");
+ for ($x = 1; $x <= count($sorted_array); $x++) {
+  $value = $sorted_array[$x];
+  echo ("$x. <a href=$value download=$value>$value</a><br>");
+ }
+}
 ?>
 
---
+++ youtube/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<button onclick="goBack()">Go Back</button>
+>
+<h1>HolyTube Downloader</h1>
+<input id="videoId" type="text"  value="<?php echo $_GET['videoId'];?>" placeholder="youtube url or videoId">
+<input id="filename" type="text" placeholder="output name" value="<?php echo $_GET['filename'];?>">
+<button onclick="download()">Download</button>
+<div id="filesize"></div>
+<script>
+var myVar;
+var filename;
+
+function goBack() {
+  window.history.back();
+}
+
+function checkDownloadProgress() {
+  myVar = setInterval(checkFileSize, 1000);
+}
+
+function checkFileSize() {
+   filename = document.getElementById("filename").value;
+
+   var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+
+         document.getElementById("filesize").innerHTML = this.responseText;
+
+      }
+    };
+    xmlhttp.open("GET", "checkfilesize.php?filename="+filename, true);
+    xmlhttp.send();
+}
+
+function download() {
+   filename = document.getElementById("filename").value;
+
+   var videoId = document.getElementById("videoId").value;
+   var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+	  	 clearInterval(myVar);
+         document.getElementById("filesize").innerHTML = this.responseText;
+		 
+      }
+    };
+    xmlhttp.open("GET", "download.php?filename="+filename+"&videoId="+videoId, true);
+    xmlhttp.send();
+	checkDownloadProgress();
+}
+</script>
+
+</body>
+</html>
