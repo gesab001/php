931ea627cea79dae109515c20c3fdb1d57b65219
root <root@phpmysql.dfheyxdluegevmrpcihrot5wjf.px.internal.cloudapp.net>
2021-06-21 01:19:25 +0000

    added select questions

--- homeschool/insert-table.php
+++ homeschool/insert-table.php
@@ -5,6 +5,7 @@ $password = "ch5t8k4u";
 $dbname = "homeschool";
 $subject = $_GET['subject'];
 $year = $_GET['year'];
+$title = $_GET['title'];
 $letter = $_GET['letter'];
 $number = $_GET['number'];
 $question = $_GET['question'];
@@ -13,7 +14,7 @@ $choice1 = $_GET['choice1'];
 $choice2 = $_GET['choice2'];
 $choice3 = $_GET['choice3'];
 $topic = $_GET['topic'];
-
+$lesson_table = $subject . "_lessons";
 //$sql =  "insert into math (id) values('1')";
 // Create connection
 
@@ -23,9 +24,10 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql =  "insert into $subject (year, letter, number, topic, question, answer, choice1, choice2, choice3) values($year, '$letter', $number, '$topic', '$question', '$answer', '$choice1', '$choice2', '$choice3')";
+$sql =  "insert into $subject (year, title, letter, number, topic, question, answer, choice1, choice2, choice3) values($year, '$title', '$letter', $number, '$topic', '$question', '$answer', '$choice1', '$choice2', '$choice3');";
+$sql .= "insert into $lesson_table(title, year, letter, number, topic, resource) values('$title', '$year', '$letter', $number, '$topic', 'url');";
 
-if ($conn->query($sql) === TRUE) {
+if ($conn->multi_query($sql) === TRUE) {
   $last_id = $conn->insert_id;
   echo "New record created successfully. Last inserted ID is: " . $last_id;
 } else {
---
+++ homeschool/select_questions.php
@@ -0,0 +1,72 @@
+<?php
+
+function cmp($a, $b) {
+    return strcmp($a->letter, $b->letter);
+}
+
+$servername = "localhost";
+$username = "gesab001";
+$password = "ch5t8k4u";
+$dbname = "homeschool";
+$tablename = $_GET['subject'];
+$year = $_GET['year'];
+$letter = $_GET['letter'];
+$number = $_GET['number'];
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "select title, year, letter, number, topic, question, answer, choice1, choice2, choice3 from $tablename where year=$year and letter='$letter' and number=$number";
+$result = $conn->query($sql);
+$rows = array();
+$categories = new stdClass();
+$activities = array();
+$myObj = new stdClass();
+$activitiesObj = new stdClass();
+while($r = mysqli_fetch_assoc($result)) {
+     $letter  = $r['letter'];
+     $title = $r['title'];
+     $number = $r['number'];
+     $key = $letter;
+     $myObj -> key  = $key;
+     $myObj -> title = $title;
+     $myObj -> letter = $letter;
+     $myObj -> activities = array();
+     if (in_array($myObj, $rows)==false){
+        $rows[] = $myObj;
+        $categories -> $key = $myObj;
+        $myObj = new stdClass();
+     }
+     
+     $topic = $r['topic'];
+     $resource = $r['resource'];
+     $activitiesObj -> key = $key;
+     $activitiesObj -> number = $number;
+     $activitiesObj -> topic = $topic;
+     $activitiesObj -> resource = $resource;
+     $activities[] = $activitiesObj;
+     $activitiesObj = new stdClass();
+
+}
+$jsonActivities = json_encode($activities);
+foreach ($activities as $value){
+  $key = $value -> key;
+  $categories ->$key -> activities [] = $value;
+}
+$result = array();
+$jsonCategories = json_encode($categories);
+foreach ($categories as $obj) {
+   unset($obj->key); 
+   $total = count($obj ->activities);  
+   for ($x = 0; $x < $total; $x++) {
+     unset($obj ->activities[$x] -> key);
+   }
+   $result[]  = $obj;
+}
+usort($result, "cmp");
+echo json_encode($result);
+$conn->close();
+?>
